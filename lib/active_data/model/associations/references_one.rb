a015f30b0547dba9df692a5a3875f5123da0a444
Evgenii Pecherkin <e.pecherkin@gmail.com>
2016-06-20 17:59:24 +0400

    fetch data in association/reference_one from reflection.persistance_adapter

--- lib/active_data/model/associations/references_one.rb
+++ lib/active_data/model/associations/references_one.rb
@@ -18,7 +18,7 @@ module ActiveData
 
         def load_target
           source = read_source
-          source ? scope(source).first : default
+          source ? reflection.persistence_adapter.find(source) : default
         end
 
         def default
@@ -30,7 +30,7 @@ module ActiveData
             when Hash
               reflection.klass.new(default)
             else
-              scope(default).first
+              reflection.persistence_adapter.find(default)
             end if default
           end
         end
@@ -63,10 +63,6 @@ module ActiveData
           target
         end
         alias_method :writer, :replace
-
-        def scope source = read_source
-          reflection.scope.where(reflection.primary_key => source)
-        end
 
         def identify
           target.try(reflection.primary_key)
